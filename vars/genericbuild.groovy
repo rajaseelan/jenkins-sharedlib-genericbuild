be1318f01f39d2b9b20b066f91ca5a2f529e1693
Rajaseelan Ganeswaran <rajaseelan@gmail.com>
2023-10-05 19:08:57 +0100

    Initial Add

---
+++ vars/genericbuild.groovy
@@ -0,0 +1,45 @@
+def call (Map config[:]) {
+    node {
+        stage('SCM') {
+            echo 'Gathering code from version control'
+            git branch: '${branch}', url: 'https://github.com/rajaseelan/jenkins-go.git'
+        }
+        stage('Build') {
+            try {
+                echo "Building Go App"
+                sh 'go version'
+                sh 'go build -o main -a main.go'
+                releasenotes(changes: "true");
+            }catch(ex) {
+                echo 'Something went wrong'
+                echo ex.toString()
+                currentBuild.result = 'FAILURE'
+            }
+            finally {
+                // cleanup
+                echo 'In finally block, executing cleanup...'
+            }
+        }
+
+        // stage('Gonna Fail') {
+        //     try {
+        //         echo 'THis will fail'
+        //         sh 'go not-a-command'
+        //     } catch(ex) {
+        //         echo 'Something Went wrong'
+        //         echo ex.toString()
+        //         //currentBuild.result = 'FAILURE'
+        //     } finally {
+        //         echo "In Finally, everything's gonna be ok"
+        //     }
+        // }
+
+        stage('Test') {
+            echo 'Testing .....'
+        }
+
+        stage('Deploy') {
+            echo 'Deploying ...'
+        }
+    }
+}
